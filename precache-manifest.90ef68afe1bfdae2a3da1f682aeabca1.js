c47b0b1e48258b7c02c366bfea408979f20d574c
BigGit823 <bigyanshrestha@gmail.com>
2020-08-09 20:57:44 -0500

    Updates

---
+++ precache-manifest.90ef68afe1bfdae2a3da1f682aeabca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdc7175c00da312dec69134ba25a79e",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "b92bfccf65684b8a462e",
+    "url": "/users/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f65f350f2d0e6e5c76f4",
+    "url": "/users/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b92bfccf65684b8a462e",
+    "url": "/users/static/js/2.ad3b6cad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/users/static/js/2.ad3b6cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65f350f2d0e6e5c76f4",
+    "url": "/users/static/js/main.b48db459.chunk.js"
+  },
+  {
+    "revision": "76492a2e6935e7af03e9",
+    "url": "/users/static/js/runtime-main.b4d70fad.js"
+  }
+]);
